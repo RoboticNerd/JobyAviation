b1485dc318bc395f0e2985d913272924791f7bba
Robot Nerd <franksebastianelli@outlook.com>
2024-10-29 08:37:18 -0700

    It works!

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 #include "classes.h"
 
 int main () {
-    std::cout<< "hello world" << std::endl;
+    int simulation_time = 180;                                      // simulation runtime
+    int vehicle_count = 20;                                         // # of vehicles in fleet
 
+    VehicleFleet fleet( 20, 180);                                   // create fleet
+
+                                                    
+    while(fleet.presentSimulationTime < fleet.maxSimulationTime){   // itterate until complete
+        fleet.moveOneMin();
+    }
+
+    fleet.finalPrint();                                             // show the results
+
+    return 0;
 }
